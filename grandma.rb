1a05321e1a0b6ea948ede23fa46d340057fa28b2
JC Chang <jerryjcchang@gmail.com>
2018-10-07 17:08:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.upcase != true
+  if phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase == true
     return "NO, NOT SINCE 1938!"
